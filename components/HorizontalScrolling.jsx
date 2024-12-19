578d85c63ee6beccc4c1935bb8faa477373276dd
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-12-19 08:56:10 +0500

    complete background image layer

--- components/HorizontalScrolling.jsx
+++ components/HorizontalScrolling.jsx
@@ -1,16 +1,64 @@
-import React from 'react';
+"use client";
+
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
 
 export default function HorizontalScrolling() {
+  useEffect(() => {
+    const backgroundImage = document.querySelector(".background-image");
+
+    // Mousemove handler
+    const handleMouseMove = (e) => {
+      const { clientX, clientY } = e;
+      const { innerWidth, innerHeight } = window;
+
+      // Calculate movement percentages
+      const moveX = (clientX / innerWidth - 0.5) * 20; // Smaller range for subtle effect
+      const moveY = (clientY / innerHeight - 0.5) * 20;
+
+      // Animate the image
+      gsap.to(backgroundImage, {
+        x: moveX,
+        y: moveY,
+        duration: 0.5, // Smooth transition
+        ease: "power2.out",
+      });
+    };
+
+    // Reset image position when the mouse leaves the screen
+    const handleMouseStop = () => {
+      gsap.to(backgroundImage, {
+        x: 0,
+        y: 0,
+        duration: 1, // Smooth reset
+        ease: "power4.out",
+      });
+    };
+
+    // Add event listeners
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseleave", handleMouseStop);
+
+    // Cleanup event listeners on unmount
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseleave", handleMouseStop);
+    };
+  }, []);
+
   return (
-    <div className="relative w-full h-screen">
-      {/* Background Layer */}
-      <div 
-        className="absolute inset-0 bg-center bg-no-repeat" 
-        style={{ 
-          backgroundImage: "url('/assest/logo.png')", 
-          backgroundSize: '60% 40%',
-          filter: 'invert(1) brightness(1)', 
-          opacity: 0.2 
+    <div className="relative w-full h-screen bg-black">
+      {/* Background Image Layer */}
+      <div
+        className="background-image absolute top-[14rem] left-[25rem]"
+        style={{
+          width: "600px", 
+          height: "300px", 
+          backgroundImage: "url('/assest/logo.png')",
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          filter: "invert(1) brightness(1)",
+          opacity: 0.8,
         }}
       ></div>
 
@@ -22,6 +70,9 @@ export default function HorizontalScrolling() {
     </div>
   );
 }
+
+
+
 
     
 
