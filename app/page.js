8c3f4b862e0a535b73d799a5057ccf724aa77769
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-12-23 15:09:05 +0500

    Add animation on horizontal section

--- app/page.js
+++ app/page.js
@@ -109,6 +109,8 @@ export default function Page() {
     });
   }, []);
 
+  
+
   return (
     <div className="h-screen w-screen overflow-hidden bg-black text-white flex flex-col">
       {/* Horizontal Scroll Section */}
